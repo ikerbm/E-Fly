ba41334e6843425f87a56c025ebecc901a556be4
Esteban <esteban.herreraa.eha@gmail.com>
2023-05-02 22:25:26 -0500

    se puso el date picker en la fecha de nacimiento

--- eflyapp/todo/forms.py
+++ eflyapp/todo/forms.py
@@ -1,19 +1,25 @@
 from django.contrib.auth.forms import UserCreationForm
 from django import forms
+from django.forms import ModelForm
 from .models import *
+from django.forms.widgets import DateInput, Select
+
 #from django.contrib.auth.models import User
 from django.contrib.auth import get_user_model
 User = get_user_model()
 
 
+class DateInput(forms.DateInput):
+    input_type = 'date'
 
-
-class CreateUsuarioForm(UserCreationForm):
+class CreateUsuarioForm(UserCreationForm, ModelForm):
+    fechaNaci = forms.DateField(widget=DateInput)
     class Meta:
         model = User
         fields = ['username','first_name','last_name','password1','password2','email',
                   'DNI','fechaNaci',
                   'lugarNaci','dirFact','sexo','tipoUsuario'] 
+                  
 class CreateAdminForm(UserCreationForm):
     class Meta:
         model = User
